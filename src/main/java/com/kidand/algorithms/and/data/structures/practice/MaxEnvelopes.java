38b4836c21ab8481d70ef773612c9e61cb7f3d86
Kidand <kidand1412@sina.com>
2021-03-04 09:28:26 +0800

    每日一题 354. 俄罗斯套娃信封问题

---
+++ src/main/java/com/kidand/algorithms/and/data/structures/practice/MaxEnvelopes.java
@@ -0,0 +1,54 @@
+package com.kidand.algorithms.and.data.structures.practice;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * ██╗  ██╗██╗██████╗  █████╗ ███╗   ██╗██████╗
+ * ██║ ██╔╝██║██╔══██╗██╔══██╗████╗  ██║██╔══██╗
+ * █████╔╝ ██║██║  ██║███████║██╔██╗ ██║██║  ██║
+ * ██╔═██╗ ██║██║  ██║██╔══██║██║╚██╗██║██║  ██║
+ * ██║  ██╗██║██████╔╝██║  ██║██║ ╚████║██████╔╝
+ * ╚═╝  ╚═╝╚═╝╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝
+ *
+ * @description: MaxEnvelopes 354. 俄罗斯套娃信封问题
+ * @author: Kidand
+ * @date: 2021/3/4 9:17
+ * Copyright © 2021 by Kidand
+ */
+public class MaxEnvelopes {
+    /**
+     * 动态规划，官方题解
+     * @param envelopes
+     * @return
+     */
+    public int maxEnvelopes(int[][] envelopes) {
+        if (envelopes.length == 0) {
+            return 0;
+        }
+
+        int n = envelopes.length;
+        Arrays.sort(envelopes, new Comparator<int[]>() {
+            public int compare(int[] e1, int[] e2) {
+                if (e1[0] != e2[0]) {
+                    return e1[0] - e2[0];
+                } else {
+                    return e2[1] - e1[1];
+                }
+            }
+        });
+
+        int[] f = new int[n];
+        Arrays.fill(f, 1);
+        int ans = 1;
+        for (int i = 1; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (envelopes[j][1] < envelopes[i][1]) {
+                    f[i] = Math.max(f[i], f[j] + 1);
+                }
+            }
+            ans = Math.max(ans, f[i]);
+        }
+        return ans;
+    }
+}
