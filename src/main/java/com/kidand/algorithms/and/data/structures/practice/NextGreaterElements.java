13372f9d401c020b87daeddbb5bf623b31f2e87a
Kidand <kidand1412@sina.com>
2021-03-07 07:55:09 +0800

    每日一题 503. 下一个更大元素 II

---
+++ src/main/java/com/kidand/algorithms/and/data/structures/practice/NextGreaterElements.java
@@ -0,0 +1,34 @@
+package com.kidand.algorithms.and.data.structures.practice;
+
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * ██╗  ██╗██╗██████╗  █████╗ ███╗   ██╗██████╗
+ * ██║ ██╔╝██║██╔══██╗██╔══██╗████╗  ██║██╔══██╗
+ * █████╔╝ ██║██║  ██║███████║██╔██╗ ██║██║  ██║
+ * ██╔═██╗ ██║██║  ██║██╔══██║██║╚██╗██║██║  ██║
+ * ██║  ██╗██║██████╔╝██║  ██║██║ ╚████║██████╔╝
+ * ╚═╝  ╚═╝╚═╝╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝
+ *
+ * @description:NextGreaterElements 503. 下一个更大元素 II
+ * @author: Kidand
+ * @date: 2021/3/7 7:53 上午
+ * Copyright © 2019-Kidand.
+ */
+public class NextGreaterElements {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] ret = new int[n];
+        Arrays.fill(ret, -1);
+        Deque<Integer> stack = new LinkedList<Integer>();
+        for (int i = 0; i < n * 2 - 1; i++) {
+            while (!stack.isEmpty() && nums[stack.peek()] < nums[i % n]) {
+                ret[stack.pop()] = nums[i % n];
+            }
+            stack.push(i % n);
+        }
+        return ret;
+    }
+}
