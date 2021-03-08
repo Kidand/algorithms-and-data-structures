fd3058d525dd88016e3b916cf91f8341cb832a3d
Kidand <kidand1412@sina.com>
2021-03-08 09:44:33 +0800

    每日一题 132. 分割回文串 II

---
+++ src/main/java/com/kidand/algorithms/and/data/structures/practice/MinCut.java
@@ -0,0 +1,53 @@
+package com.kidand.algorithms.and.data.structures.practice;
+
+import java.util.Arrays;
+
+/**
+ * ██╗  ██╗██╗██████╗  █████╗ ███╗   ██╗██████╗
+ * ██║ ██╔╝██║██╔══██╗██╔══██╗████╗  ██║██╔══██╗
+ * █████╔╝ ██║██║  ██║███████║██╔██╗ ██║██║  ██║
+ * ██╔═██╗ ██║██║  ██║██╔══██║██║╚██╗██║██║  ██║
+ * ██║  ██╗██║██████╔╝██║  ██║██║ ╚████║██████╔╝
+ * ╚═╝  ╚═╝╚═╝╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝
+ *
+ * @description: MinCut 132. 分割回文串 II
+ * @author: Kidand
+ * @date: 2021/3/8 9:41
+ * Copyright © 2021 by Kidand
+ */
+public class MinCut {
+    /**
+     * 力扣题解
+     * @param s
+     * @return
+     */
+    public int minCut(String s) {
+        int n = s.length();
+        boolean[][] g = new boolean[n][n];
+        for (int i = 0; i < n; ++i) {
+            Arrays.fill(g[i], true);
+        }
+
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = i + 1; j < n; ++j) {
+                g[i][j] = s.charAt(i) == s.charAt(j) && g[i + 1][j - 1];
+            }
+        }
+
+        int[] f = new int[n];
+        Arrays.fill(f, Integer.MAX_VALUE);
+        for (int i = 0; i < n; ++i) {
+            if (g[0][i]) {
+                f[i] = 0;
+            } else {
+                for (int j = 0; j < i; ++j) {
+                    if (g[j + 1][i]) {
+                        f[i] = Math.min(f[i], f[j] + 1);
+                    }
+                }
+            }
+        }
+
+        return f[n - 1];
+    }
+}
