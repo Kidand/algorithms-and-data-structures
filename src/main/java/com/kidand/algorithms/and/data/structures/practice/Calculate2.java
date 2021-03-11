44fbe2a503b3f957b13bcef71a4ff0e0683f2dab
Kidand <kidand1412@sina.com>
2021-03-11 11:02:52 +0800

    每日一题 227. 基本计算器 II

---
+++ src/main/java/com/kidand/algorithms/and/data/structures/practice/Calculate2.java
@@ -0,0 +1,53 @@
+package com.kidand.algorithms.and.data.structures.practice;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * ██╗  ██╗██╗██████╗  █████╗ ███╗   ██╗██████╗
+ * ██║ ██╔╝██║██╔══██╗██╔══██╗████╗  ██║██╔══██╗
+ * █████╔╝ ██║██║  ██║███████║██╔██╗ ██║██║  ██║
+ * ██╔═██╗ ██║██║  ██║██╔══██║██║╚██╗██║██║  ██║
+ * ██║  ██╗██║██████╔╝██║  ██║██║ ╚████║██████╔╝
+ * ╚═╝  ╚═╝╚═╝╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝
+ *
+ * @description: Calculate2 227. 基本计算器 II
+ * @author: Kidand
+ * @date: 2021/3/11 9:36
+ * Copyright © 2021 by Kidand
+ */
+public class Calculate2 {
+    public int calculate(String s) {
+        Deque<Integer> stack = new LinkedList<Integer>();
+        char preSign = '+';
+        int num = 0;
+        int n = s.length();
+        for (int i = 0; i < n; ++i) {
+            if (Character.isDigit(s.charAt(i))) {
+                num = num * 10 + s.charAt(i) - '0';
+            }
+            if (!Character.isDigit(s.charAt(i)) && s.charAt(i) != ' ' || i == n - 1) {
+                switch (preSign) {
+                    case '+':
+                        stack.push(num);
+                        break;
+                    case '-':
+                        stack.push(-num);
+                        break;
+                    case '*':
+                        stack.push(stack.pop() * num);
+                        break;
+                    default:
+                        stack.push(stack.pop() / num);
+                }
+                preSign = s.charAt(i);
+                num = 0;
+            }
+        }
+        int ans = 0;
+        while (!stack.isEmpty()) {
+            ans += stack.pop();
+        }
+        return ans;
+    }
+}
