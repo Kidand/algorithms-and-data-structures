9c8423ee25be4263c9bb0afc4ed5940387f66c46
Kidand <kidand1412@sina.com>
2021-03-18 09:32:40 +0800

    每日一题 92. 反转链表 II

---
+++ src/main/java/com/kidand/algorithms/and/data/structures/practice/ReverseBetween.java
@@ -0,0 +1,57 @@
+package com.kidand.algorithms.and.data.structures.practice;
+
+/**
+ * ██╗  ██╗██╗██████╗  █████╗ ███╗   ██╗██████╗
+ * ██║ ██╔╝██║██╔══██╗██╔══██╗████╗  ██║██╔══██╗
+ * █████╔╝ ██║██║  ██║███████║██╔██╗ ██║██║  ██║
+ * ██╔═██╗ ██║██║  ██║██╔══██║██║╚██╗██║██║  ██║
+ * ██║  ██╗██║██████╔╝██║  ██║██║ ╚████║██████╔╝
+ * ╚═╝  ╚═╝╚═╝╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝
+ *
+ * @description: ReverseBetween 92. 反转链表 II
+ * @author: Kidand
+ * @date: 2021/3/18 9:28
+ * Copyright © 2021 by Kidand
+ */
+public class ReverseBetween {
+    /**
+     * Definition for singly-linked list.
+     */
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    ListNode successor = null;
+
+    public ListNode reverseN(ListNode head, int m) {
+        if (m == 1) {
+            successor = head.next;
+            return head;
+        }
+        ListNode last = reverseN(head.next, m - 1);
+        head.next.next = head;
+        head.next = successor;
+        return last;
+    }
+
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        if (m == 1) {
+            return reverseN(head, n);
+        }
+        head.next = reverseBetween(head.next, m - 1, n - 1);
+        return head;
+    }
+}
