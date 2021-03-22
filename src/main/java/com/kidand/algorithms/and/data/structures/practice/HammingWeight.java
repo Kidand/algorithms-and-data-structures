ad8a603b8ed52d264260d1dafda0397a896b40e8
Kidand <kidand1412@sina.com>
2021-03-22 09:02:14 +0800

    每日一题 191. 位1的个数

---
+++ src/main/java/com/kidand/algorithms/and/data/structures/practice/HammingWeight.java
@@ -0,0 +1,29 @@
+package com.kidand.algorithms.and.data.structures.practice;
+
+/**
+ * ██╗  ██╗██╗██████╗  █████╗ ███╗   ██╗██████╗
+ * ██║ ██╔╝██║██╔══██╗██╔══██╗████╗  ██║██╔══██╗
+ * █████╔╝ ██║██║  ██║███████║██╔██╗ ██║██║  ██║
+ * ██╔═██╗ ██║██║  ██║██╔══██║██║╚██╗██║██║  ██║
+ * ██║  ██╗██║██████╔╝██║  ██║██║ ╚████║██████╔╝
+ * ╚═╝  ╚═╝╚═╝╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝
+ *
+ * @description: HammingWeight 191. 位1的个数
+ * @author: Kidand
+ * @date: 2021/3/22 8:59
+ * Copyright © 2021 by Kidand
+ */
+public class HammingWeight {
+    /**
+     * you need to treat n as an unsigned value
+     */
+    public int hammingWeight(int n) {
+        int ret = 0;
+        for (int i = 0; i < 32; i++) {
+            if ((n & (1 << i)) != 0) {
+                ret++;
+            }
+        }
+        return ret;
+    }
+}
