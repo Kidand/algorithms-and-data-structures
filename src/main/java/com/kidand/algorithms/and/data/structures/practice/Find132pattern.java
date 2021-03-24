34d5b31381694dc58c06edda0c8949ea2a75705b
Kidand <kidand1412@sina.com>
2021-03-24 08:46:22 +0800

    每日一题 456. 132模式

---
+++ src/main/java/com/kidand/algorithms/and/data/structures/practice/Find132pattern.java
@@ -0,0 +1,50 @@
+package com.kidand.algorithms.and.data.structures.practice;
+
+import java.util.TreeMap;
+
+/**
+ * ██╗  ██╗██╗██████╗  █████╗ ███╗   ██╗██████╗
+ * ██║ ██╔╝██║██╔══██╗██╔══██╗████╗  ██║██╔══██╗
+ * █████╔╝ ██║██║  ██║███████║██╔██╗ ██║██║  ██║
+ * ██╔═██╗ ██║██║  ██║██╔══██║██║╚██╗██║██║  ██║
+ * ██║  ██╗██║██████╔╝██║  ██║██║ ╚████║██████╔╝
+ * ╚═╝  ╚═╝╚═╝╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝
+ *
+ * @description: Find132pattern 456. 132模式
+ * @author: Kidand
+ * @date: 2021/3/24 8:45
+ * Copyright © 2021 by Kidand
+ */
+public class Find132pattern {
+    public boolean find132pattern(int[] nums) {
+        int n = nums.length;
+        if (n < 3) {
+            return false;
+        }
+
+        // 左侧最小值
+        int leftMin = nums[0];
+        // 右侧所有元素
+        TreeMap<Integer, Integer> rightAll = new TreeMap<Integer, Integer>();
+
+        for (int k = 2; k < n; ++k) {
+            rightAll.put(nums[k], rightAll.getOrDefault(nums[k], 0) + 1);
+        }
+
+        for (int j = 1; j < n - 1; ++j) {
+            if (leftMin < nums[j]) {
+                Integer next = rightAll.ceilingKey(leftMin + 1);
+                if (next != null && next < nums[j]) {
+                    return true;
+                }
+            }
+            leftMin = Math.min(leftMin, nums[j]);
+            rightAll.put(nums[j + 1], rightAll.get(nums[j + 1]) - 1);
+            if (rightAll.get(nums[j + 1]) == 0) {
+                rightAll.remove(nums[j + 1]);
+            }
+        }
+
+        return false;
+    }
+}
