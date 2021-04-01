700408d2dab6abdde93ede25f53972eaeb93b851
Kidand <kidand1412@sina.com>
2021-04-01 09:07:47 +0800

    每日一题 1006. 笨阶乘

---
+++ src/main/java/com/kidand/algorithms/and/data/structures/practice/Clumsy.java
@@ -0,0 +1,48 @@
+package com.kidand.algorithms.and.data.structures.practice;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * ██╗  ██╗██╗██████╗  █████╗ ███╗   ██╗██████╗
+ * ██║ ██╔╝██║██╔══██╗██╔══██╗████╗  ██║██╔══██╗
+ * █████╔╝ ██║██║  ██║███████║██╔██╗ ██║██║  ██║
+ * ██╔═██╗ ██║██║  ██║██╔══██║██║╚██╗██║██║  ██║
+ * ██║  ██╗██║██████╔╝██║  ██║██║ ╚████║██████╔╝
+ * ╚═╝  ╚═╝╚═╝╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝
+ *
+ * @description: Clumsy 1006. 笨阶乘
+ * @author: Kidand
+ * @date: 2021/4/1 9:06
+ * Copyright © 2021 by Kidand
+ */
+public class Clumsy {
+    public int clumsy(int N) {
+        Deque<Integer> stack = new LinkedList<Integer>();
+        stack.push(N);
+        N--;
+
+        // 用于控制乘、除、加、减
+        int index = 0;
+        while (N > 0) {
+            if (index % 4 == 0) {
+                stack.push(stack.pop() * N);
+            } else if (index % 4 == 1) {
+                stack.push(stack.pop() / N);
+            } else if (index % 4 == 2) {
+                stack.push(N);
+            } else {
+                stack.push(-N);
+            }
+            index++;
+            N--;
+        }
+
+        // 把栈中所有的数字依次弹出求和
+        int sum = 0;
+        while (!stack.isEmpty()) {
+            sum += stack.pop();
+        }
+        return sum;
+    }
+}
